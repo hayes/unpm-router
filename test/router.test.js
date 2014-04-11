8d4e64174794655745402dc6faae52bfa61b2b6b
jesse keane <me@jessekeane.me>
2014-04-11 13:42:44 -0700

    add tests

--- test/router.test.js
+++ test/router.test.js
@@ -2,6 +2,8 @@ var Router = require('../')
   , test = require('tape')
 
 test('does not match routes with unmatching methods', function(t) {
+  t.plan(1)
+
   var router = Router()
 
   var fake_req = {method: 'post', url: '/cats'}
@@ -9,8 +11,30 @@ test('does not match routes with unmatching methods', function(t) {
   router.add('get', '/cats', noop)
 
   t.ok(!router.match(fake_req), 'routes scoped by method')
-  
-  t.end()
+})
+
+test('routes are prefixable', function(t) {
+  t.plan(1)
+
+  var router = Router('/lol/')
+
+  var fake_req = {method: 'get', url: '/lol/cats'}
+
+  router.add('get', '/cats', noop)
+
+  t.ok(router.match(fake_req), 'routes work behind prefix')
+})
+
+test('prefix works with / route', function(t) {
+  t.plan(1)
+
+  var router = Router('/lol/')
+
+  var fake_req = {method: 'get', url: '/lol/'}
+
+  router.add('get', '/', noop)
+
+  t.ok(router.match(fake_req), '/ route works with prefix')
 })
 
 function noop() {
